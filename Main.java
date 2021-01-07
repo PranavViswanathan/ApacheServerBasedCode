acbb4784bc957cf07544e98ea73a100a6240cd8d
Pranav Viswanathan <72350161+PranavViswanathan@users.noreply.github.com>
2021-01-07 11:51:22 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,9 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class PostExample
- */
+
 @WebServlet("/PostExample")
 public class PostExample extends HttpServlet {
 	private static final long serialVersionUID = 1L;
